53850d50d28c3bb34500676b4895358f3906ad21
hamedshahidi <hamed.shahidiz@gmail.com>
2019-03-02 14:42:56 +0200

    Fetches profile info. ( adds profile image , gets all user posts )

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,43 +1,66 @@
 import { Component } from '@angular/core'
 import { NavController } from 'ionic-angular'
 import { UserProvider } from '../../providers/user/user'
-import { User } from '../../interfaces/Media'
-import {SavedAddsPage} from '../saved-adds/saved-adds'
+import { TagReponse, User } from '../../interfaces/Media'
+import { SavedAddsPage } from '../saved-adds/saved-adds'
 import { OwnJobsPage } from '../own-jobs/own-jobs'
 import { SentOffersPage } from '../sent-offers/sent-offers'
-// import { MyOpenOffersPage } from '../my-open-offers/my-open-offers';
-// import { JobInfoOfferedPage } from '../job-info-offered/job-info-offered';
-// import { HomePage } from '../home/home';
-// import { JobInfoPage } from '../job-info/job-info';
-// import { ProfileView2Page } from '../profile-view2/profile-view2';
-// import { Reviews2Page } from '../reviews2/reviews2';
-// import { UsersOpenJobsPage } from '../users-open-jobs/users-open-jobs';
-// import { JobInfo2Page } from '../job-info2/job-info2';
-// import { SendOffer2Page } from '../send-offer2/send-offer2';
-// import { OfferSentPage } from '../offer-sent/offer-sent';
-// import { SendOfferPage } from '../send-offer/send-offer';
-// import { ViewerPage } from '../viewer/viewer';
-// import { MyOpenJobsPage } from '../my-open-jobs/my-open-jobs';
-// import { MyJobInfoPage } from '../my-job-info/my-job-info';
-// import { OffersPage } from '../offers/offers';
-// import { Page10Page } from '../page10/page10';
-// import { ReviewsPage } from '../reviews/reviews';
-// import { AcceptOfferPage } from '../accept-offer/accept-offer';
-// import { MySavedAddsPage } from '../my-saved-adds/my-saved-adds';
+import { MediaProvider } from '../../providers/media/media'
+import { stringify } from 'querystring'
 
 @Component({
   selector: 'page-my-profile',
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-  user: User
+
+  mediaFilePath = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  private user: User
+  private username: string
+//  private userId: string
+  private userId = 18
+  private email: string
+  private projectFilesArray: TagReponse[]
+  private userFilesArray: any
+  private profileImage: TagReponse
+  avatar: string
 
   constructor (
     public navCtrl: NavController,
     public userProvider: UserProvider,
+    public mediaProvider: MediaProvider,
   ) {
   }
 
+  /*  ionViewDidLoad () {
+      this.getProfileInfo()
+    }*/
+
+  ionViewWillEnter () {
+    this.getProfileInfo()
+  }
+
+  // ========================
+  //        Navigation
+  // ========================
+  goToSavedAdds (params) {
+    if (!params) params = {}
+    this.navCtrl.push(SavedAddsPage)
+  }
+
+  goToMyOwnJobs (params) {
+    if (!params) params = {}
+    this.navCtrl.push(OwnJobsPage)
+  }
+
+  goToMySentOffers (params) {
+    if (!params) params = {}
+    this.navCtrl.push(SentOffersPage)
+  }
+
+  // ========================
+  //        Functions
+  // ========================
   logout () {
     localStorage.clear()
     this.userProvider.loggedIn = false
@@ -47,76 +70,58 @@ export class ProfilePage {
     this.navCtrl.parent.select(1)
   }
 
-  /*  goToMyOpenOffers(params){
-      if (!params) params = {};
-      this.navCtrl.push(MyOpenOffersPage);
-    }goToJobInfoOffered(params){
-      if (!params) params = {};
-      this.navCtrl.push(JobInfoOfferedPage);
-    }goToHome(params){
-      if (!params) params = {};
-      this.navCtrl.push(HomePage);
-    }goToJobInfo(params){
-      if (!params) params = {};
-      this.navCtrl.push(JobInfoPage);
-    }goToProfileView2(params){
-      if (!params) params = {};
-      this.navCtrl.push(ProfileView2Page);
-    }goToReviews2(params){
-      if (!params) params = {};
-      this.navCtrl.push(Reviews2Page);
-    }goToUsersOpenJobs(params){
-      if (!params) params = {};
-      this.navCtrl.push(UsersOpenJobsPage);
-    }goToJobInfo2(params){
-      if (!params) params = {};
-      this.navCtrl.push(JobInfo2Page);
-    }goToSendOffer2(params){
-      if (!params) params = {};
-      this.navCtrl.push(SendOffer2Page);
-    }goToOfferSent(params){
-      if (!params) params = {};
-      this.navCtrl.push(OfferSentPage);
-    }goToSendOffer(params){
-      if (!params) params = {};
-      this.navCtrl.push(SendOfferPage);
-    }goToViewer(params){
-      if (!params) params = {};
-      this.navCtrl.push(ViewerPage);
-    }goToMyOpenJobs(params){
-      if (!params) params = {};
-      this.navCtrl.push(MyOpenJobsPage);
-    }goToMyJobInfo(params){
-      if (!params) params = {};
-      this.navCtrl.push(MyJobInfoPage);
-    }goToOffers(params){
-      if (!params) params = {};
-      this.navCtrl.push(OffersPage);
-    }goToPage10(params){
-      if (!params) params = {};
-      this.navCtrl.push(Page10Page);
-    }goToReviews(params){
-      if (!params) params = {};
-      this.navCtrl.push(ReviewsPage);
-    }goToAcceptOffer(params){
-      if (!params) params = {};
-      this.navCtrl.push(AcceptOfferPage);
-    }goToMySavedAdds(params){
-      if (!params) params = {};
-      this.navCtrl.push(MySavedAddsPage);
-    }*/
-  goToSavedAdds (params) {
-    if (!params) params = {};
-    this.navCtrl.push(SavedAddsPage);
+  getProfileInfo () {
+    this.username = localStorage.getItem('username')
+    this.userId = parseInt(localStorage.getItem('user_id'))
+    this.email = localStorage.getItem('email')
+
+    this.getProfileImage()
+    this.getUserFilesByUserId()
+    //this.getProjectFiles()
   }
 
-  goToMyOwnJobs (params) {
-    if (!params) params = {};
-    this.navCtrl.push(OwnJobsPage);
+  private getProjectFiles () {
+    this.mediaProvider.getFilesByTag('freelancer').subscribe(
+      (response: TagReponse[]) => {
+        this.projectFilesArray = response
+        console.log('User id in profile page = ' + this.userId)
+      },
+    )
   }
 
-  goToMySentOffers (params) {
-    if (!params) params = {};
-    this.navCtrl.push(SentOffersPage);
+  // get user files from all project files
+  private getUserFilesFromProjectFiles () {
+    this.getProjectFiles()
+    this.userFilesArray = this.projectFilesArray.filter(file =>
+      file.user_id === this.userId,
+    )
+  }
+
+  private getUserFilesByUserId () {
+    this.mediaProvider.getFilesByUserId(this.userId).subscribe(
+      result => {
+        this.userFilesArray = result
+        console.log(result)
+      }, error => {
+        console.log(error)
+      },
+    )
+  }
+
+  private getProfileImage () {
+    this.mediaProvider.getFilesByTag('profile').subscribe(
+      (response: TagReponse[]) => {
+          response.forEach(file => {
+            if(file.user_id === this.userId){
+              this.profileImage = file
+              this.avatar = file.file_id.toString();
+              console.log('avatar id: ' + this.avatar);
+            }
+          })
+      },
+      error => {
+        console.log(error)
+      },
+    )
   }
 }
--- src/providers/media/media.ts
+++ src/providers/media/media.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Media, TagReponse } from '../../interfaces/Media'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Media, TagReponse, User } from '../../interfaces/Media'
 
 /*
   Generated class for the MediaProvider provider.
@@ -11,17 +11,37 @@ import { Media, TagReponse } from '../../interfaces/Media'
 @Injectable()
 export class MediaProvider {
 
-  mediaAPI = 'http://media.mw.metropolia.fi/wbma/';
+  mediaAPI = 'http://media.mw.metropolia.fi/wbma/'
 
-  constructor(public http: HttpClient) {
-    console.log('Hello MediaProvider Provider');
+  constructor (public http: HttpClient) {
+    console.log('Hello MediaProvider Provider')
   }
 
-  getSingleMedia(id) {
-    return this.http.get<Media>(this.mediaAPI + 'media/' + id);
+  getSingleMedia (id) {
+    return this.http.get<Media>(this.mediaAPI + 'media/' + id)
   }
 
-  getFilesByTag(tag: string) {
-    return this.http.get<TagReponse[]>(this.mediaAPI + 'tags/' + tag);
+  getFilesByTag (tag: string) {
+    return this.http.get<TagReponse[]>(this.mediaAPI + 'tags/' + tag)
+  }
+
+  getFilesByUserId (id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'x-access-token': localStorage.getItem('token'),
+      }),
+    }
+    return this.http.get<Media[]>(this.mediaAPI + 'media/user/' + id,
+      httpOptions)
+  }
+
+  requestUserInfo (id: number) {
+    const httpOtions = {
+      headers: new HttpHeaders({
+        'x-access-token': localStorage.getItem('token'),
+      }),
+    }
+    return this.http.get<User>(this.mediaAPI + 'users/' + id,
+      httpOtions)
   }
 }
