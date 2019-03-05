7fa96cd04b6a6bb8e70856f95ffa580fbf4b642b
hamedshahidi <hamed.shahidiz@gmail.com>
2019-03-05 18:16:07 +0200

    login functionality added to signing up process.

--- src/providers/media/media.ts
+++ src/providers/media/media.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Media, TagReponse, User } from '../../interfaces/Media'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Media, TagReponse, User } from '../../interfaces/Media';
 
 /*
   Generated class for the MediaProvider provider.
@@ -13,35 +13,35 @@ export class MediaProvider {
 
   mediaAPI = 'http://media.mw.metropolia.fi/wbma/';
 
-  constructor (public http: HttpClient) {
-    console.log('Hello MediaProvider Provider')
+  constructor(public http: HttpClient) {
+    console.log('Hello MediaProvider Provider');
   }
 
-  getSingleMedia (id) {
-    return this.http.get<Media>(this.mediaAPI + 'media/' + id)
+  getSingleMedia(id) {
+    return this.http.get<Media>(this.mediaAPI + 'media/' + id);
   }
 
-  getFilesByTag (tag: string) {
-    return this.http.get<TagReponse[]>(this.mediaAPI + 'tags/' + tag)
+  getFilesByTag(tag: string) {
+    return this.http.get<TagReponse[]>(this.mediaAPI + 'tags/' + tag);
   }
 
-  getFilesByUserId (id: number) {
+  getFilesByUserId(id: number) {
     const httpOptions = {
       headers: new HttpHeaders({
         'x-access-token': localStorage.getItem('token'),
       }),
     };
     return this.http.get<Media[]>(this.mediaAPI + 'media/user/' + id,
-      httpOptions)
+      httpOptions);
   }
 
-  requestUserInfo (id: number) {
+  requestUserInfo(id: number) {
     const httpOtions = {
       headers: new HttpHeaders({
         'x-access-token': localStorage.getItem('token'),
       }),
     };
     return this.http.get<User>(this.mediaAPI + 'users/' + id,
-      httpOtions)
+      httpOtions);
   }
 }
