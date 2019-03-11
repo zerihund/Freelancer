6a38774f5dd68abd03a1fc09f2a3facf85cf7990
hamedshahidi <hamed.shahidiz@gmail.com>
2019-03-11 16:03:59 +0200

    Splash screen and app icon added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,30 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-
+import { timer } from 'rxjs/observable/timer';
 
 import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
 
-
-
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
-  
-  rootPage:any = TabsControllerPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = TabsControllerPage;
+  showSplash = true;
+
+  constructor(
+    platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      timer(3000).subscribe(() => {
+        this, this.showSplash = false;
+      });
     });
   }
-  
+
 }
